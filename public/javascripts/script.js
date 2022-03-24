04f3418250dba189c8401d2a6f8b7746aa287f56
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2022-03-24 17:07:46 +0700

    updated

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,27 +4,6 @@ function getNameFile() {
     document.getElementById('file-input').innerHTML = result;
 }
 
-// function setCSS() {
-//     var currTabElem1 = document.getElementById('show-col-0');
-//     let currentClassName1 = document.getElementById("show-col-0").className;
-
-//     if (currentClassName1 == "col-0") {
-//         currTabElem1.setAttribute("class", "col-2");
-//     } else {
-//         currTabElem1.setAttribute("class", "col-0");
-//     }
-
-//     var currTabElem2 = document.getElementById('show-col-12');
-//     let currentClassName2 = document.getElementById("show-col-12").className;
-
-//     if (currentClassName2 == "col-12") {
-//         currTabElem2.setAttribute("class", "col-10");
-//     } else {
-//         currTabElem2.setAttribute("class", "col-12");
-//     }
-// }
-
-
 $('#toggleSidebar').click(function(){
     $('.collapseSidebar').toggle();
 
@@ -46,15 +25,3 @@ $('#toggleSidebar').click(function(){
         currTabElem2.setAttribute("class", "col-lg-12");
     }
  });
-
-
-
-// $(document).ready(function(){
-//     $("button").click(function(){
-//       if(1+1 == 3){
-//       $("h1").removeClass();
-//       }
-        
-//         $("p").removeClass();
-//     });
-// });
