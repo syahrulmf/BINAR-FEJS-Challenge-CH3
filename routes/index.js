46f44d9254cc2c3275d70a14ff32a11337502af8
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2022-03-25 16:06:16 +0700

    move to ejs layout

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function(req, res) {
+  res.render('index', {layout : false, title : 'BCR - Login'});
 });
 
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard');
+router.get('/dashboard', function(req, res) {
+  res.render('dashboard', {title : 'BCR - Dashboard'});
 });
 
-router.get('/list-car', function(req, res, next) {
-  res.render('listCar');
+router.get('/list-car', function(req, res) {
+  res.render('listCar', {title : 'BCR - List Car'});
 });
 
-router.get('/add-new-car', function(req, res, next) {
-  res.render('newCar');
+router.get('/add-new-car', function(req, res) {
+  res.render('newCar', {title : 'BCR - Add New Car'});
 });
 
 module.exports = router;
